fe28e1d9a9a73f2dc6fd2ca7854eacc9388e47f9
BAKIPAVAN <pavanbaki.p@outlook.com>
2023-12-09 00:02:04 +0530

    button changes

--- Mono-repo-workspace/projects/host-app/src/app/home/home.component.ts
+++ Mono-repo-workspace/projects/host-app/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+
+  constructor(public route :Router){
+
+  }
+  callremotemodule() {
+    this.route.navigate(['todo-list']);
+  }
 
 }
